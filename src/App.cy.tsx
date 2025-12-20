5de35095b79755ea4d4c4abde1885b941feec753
arturshilnikov <shilnikov.ad@phystech.edu>
2025-12-20 17:01:30 +0300

    demo

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { mount } from '@cypress/react'
 import App from './App'
 
 describe('<App />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
+    mount(<App />)
   })
 })
